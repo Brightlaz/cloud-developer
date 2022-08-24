fc9bf3b7f66a958fe57acf6d097fc6bc14e17212
Bright Lazarus <lazbright1@gmail.com>
2022-08-24 16:17:43 +0100

    updated security policy

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,16 +1,16 @@
 export const config = {
   "dev": {
-    "username": "udagram",
-    "password": "123bright%^*",
-    "database": "udagram",
-    "host": "udagram.cmh6oagnugu1.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": "helloWorld"
   },
   "prod": {
     "username": "",
