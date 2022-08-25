cedb8f3f69f88ea46df465b1df11245c49471aef
Bright Lazarus <lazbright1@gmail.com>
2022-08-25 15:42:14 +0100

    Debugging
    I fixed the error in my code

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRES_USERNAME,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DATABASE,
-    "host": process.env.POSTGRES_HOST,
+    "username": 'udagram',
+    "password": '123bright%^*',
+    "database": 'udagram',
+    "host": 'udagram.cmh6oagnugu1.us-east-1.rds.amazonaws.com',
     "dialect": "postgres",
-    "aws_region": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    "aws_region": 'us-east-2',
+    "aws_profile": 'default',
+    "aws_media_bucket": 'udagram-ruttner-dev'
   },
   "jwt": {
     "secret": " "
@@ -20,3 +20,27 @@ export const config = {
     "dialect": "postgres"
   }
 }
+
+
+// export const config = {
+//   "dev": {
+//     "username": process.env.POSTGRES_USERNAME,
+//     "password": process.env.POSTGRES_PASSWORD,
+//     "database": process.env.POSTGRES_DATABASE,
+//     "host": process.env.POSTGRES_HOST,
+//     "dialect": "postgres",
+//     "aws_region": process.env.AWS_REGION,
+//     "aws_profile": process.env.AWS_PROFILE,
+//     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+//   },
+//   "jwt": {
+//     "secret": " "
+//   },
+//   "prod": {
+//     "username": "",
+//     "password": "",
+//     "database": "udagram_prod",
+//     "host": "",
+//     "dialect": "postgres"
+//   }
+// }
